de78f9353290174c7a32df6310d91b86771b7e63
Matthew Harwood <matthhar12@gmail.com>
2015-06-30 14:45:46 -0700

    added the serveHTTP custom

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
 	"log"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		log.Println(r)
-		w.Write([]byte("hello world"))
-	})
+	http.Handle("/", new(MyHandler))
 
 	http.ListenAndServe(":8000", nil)
+}
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
 }
