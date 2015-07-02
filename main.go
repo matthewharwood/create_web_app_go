228084891ed26e0f859aacb94ac045a5a76c2684
Matthew Harwood <matthhar12@gmail.com>
2015-07-02 10:41:28 -0700

    added first template

--- main.go
+++ main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Content Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+		if err == nil {
+			tmpl.Execute(w, nil)
+		}
+	})
+	http.ListenAndServe(":8000", nil)
 }
+
+const doc  = `
+<!DOCTYPE>
+<html>
+	<head>
+		<title>example</title>
+	</head>
+	<body>
+		<h1>heyhey</h1>
+	</body>
+</html>
+
+`
 
