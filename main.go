be8634900d34b4ddb07187bb3d96ea538bee2472
Matthew Harwood <matthhar12@gmail.com>
2015-07-03 04:33:55 -0700

    added proper templating for sub templates

--- main.go
+++ main.go
@@ -8,30 +8,46 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Add("Content Type", "text/html")
-		tmpl, err := template.New("test").Parse(doc)
-		if err == nil {
-			context := Context{"message wow"}
-			tmpl.Execute(w, context)
+		templates := template.New("template")
+		templates.New("test").Parse(doc)
+		templates.New("header").Parse(header)
+		templates.New("footer").Parse(footer)
+		context := Context{
+			[3]string{"lemon", "apple", "banana"},
+			"the title",
+
 		}
+		templates.Lookup("test").Execute(w, context)
+
 	})
 	http.ListenAndServe(":8000", nil)
 }
 
-const doc  = `
-<!DOCTYPE>
-<html>
-	<head>
-		<title>example</title>
-	</head>
-	<body>
+const doc = `
+{{template "header" .Message}}
+  <body>
+    <h1>List of Fruit</h1>
+    <ul>
+      {{range .Fruit}}
+      	<li>{{.}}</li>
+      {{end}}
+    </ul>
+  </body>
+{{template "footer"}}
+`
+const  header =  `
+    <body>
 		<h1>heyhey</h1>
-		<h1>{{.Message}}</h1>
+		<h1>{{.}}</h1>
 	</body>
-</html>
-
 `
 
+const  footer = `
+   <p> i'm a footer</p>
+    </html>
+`
 type Context struct {
+	Fruit [3]string
 	Message string
 }
 
